a3acf0919782cc098216a1d9ad50577476c22fa2
EldorPardaevDeveloper <eldorpardayev18@gmail.com>
2023-12-21 00:02:24 +0500

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -12,3 +12,12 @@ System.Console.WriteLine($"successfuly converted! {age}");
 int keschasAge = 3;
 int ageDifference = age - keschasAge;
 System.Console.WriteLine($"The difference between your and Kescha's age is {ageDifference}");
+
+// > >= < <= == !=
+
+System.Console.WriteLine($"You are older than KESCHA: {age > keschasAge}");
+System.Console.WriteLine($"You are younger than KESCHA: {age < keschasAge}");
+System.Console.WriteLine($"You are older than or equal to KESCHA: {age >= keschasAge}");
+System.Console.WriteLine($"You are less than or equal to KESCHA: {age <= keschasAge}");
+System.Console.WriteLine($"Are you equal to KESCHA: {age == keschasAge}");
+System.Console.WriteLine($"Are you no match for KESCHA: {age != keschasAge}");
